858e07f65b002f9301da4632b235988ad931738f
agnesarth <agnesarth@hotmail.fr>
2020-04-21 22:41:29 +0200

    ajout app_3 non fini

--- app_3.rb
+++ app_3.rb
@@ -3,3 +3,9 @@ Bundler.require
 
 require_relative 'lib/game'
 require_relative 'lib/player'
+
+
+villain1 = Player.new("Josiane")
+villain2 = Player.new("José")
+villain3 = Player.new("Jorgina")
+villain4 = Player.new("Josélito")
--- lib/game.rb
+++ lib/game.rb
@@ -1,21 +1,29 @@
-require 'pry'
+require 'bundler'
+Bundler.require
+
 
 class Game
   require_relative 'player'
   attr_accessor :human_player, :villains
-  @@all_villains = []
-  def initialize(name_to_save)
-    villain1 = Player.new("Josiane")
-    villain2 = Player.new("José")
-    villain3 = Player.new("Jorgina")
-    villain4 = Player.new("Josélito")
-    @@all_villains = [villain1, villain2, villain3, villain4]
-    @human_player = HumanPlayer.new("#{name_to_save}")
+
+  villain1 = Player.new("Josiane")
+  villain2 = Player.new("José")
+  villain3 = Player.new("Jorgina")
+  villain4 = Player.new("Josélito")
+  @villains = [villain1, villain2, villain3, villain4] #
+
+  def initialize (name_to_save) #moi = Game.new("Hercule")
+    @human_player = HumanPlayer.new("#{name_to_save}") #test avec moi.human_player.show_state
   end
 
-  def kill_player(all_villains)
-    @@all_villains.delete(@@all_villains.name)
+  def kill_player(player_to_kill) #test avec moi.kill_player("villain1")
+    @villains.each do |player|
+      @villains.delete(player) if player.name == player_to_kill
+      return @villains
+    end
   end
+
+
 
 binding.pry
 end
